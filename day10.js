7b42d10d4c9facacf68437881d4f2654389af713
Eric Lifka <eric.lifka@inin.com>
2017-12-11 20:51:37 -0500

    day 10 part 2

--- day10.js
+++ day10.js
@@ -1,18 +1,41 @@
-const getList = () => Array.apply(null, Array(256)).map((_, i) => i)
-const getJumps = () => 
+const pad = str =>
+    str.length >= 2 ? str :
+        `0${str}`
+
+const tohex = num =>
+    pad(num.toString(16).toLowerCase())
+
+const numberArray = size =>
+    [...Array(size)].map((_, index) => index)
+
+const getCharCode = letter =>
+    letter.charCodeAt(0)
+
+const xor = nums =>
+    nums.reduce((xor, num) => num ^ xor)
+
+const chunkArray = array =>
+    array.length === 0 ? array :
+        [ array.slice(0, 16),
+          ...chunkArray(array.slice(16)) ]
+
+const parseInput = () =>
     `94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243`
-        .split(',')
-        .map(n => parseInt(n, 10))
+        .split('')
+        .map(getCharCode)
+
+const getHashJumps = () =>
+    [ ...parseInput(), 17, 31, 73, 47, 23 ]
 
 const nodeify = (parent, [number, ...rest], node = { number }) => (
     parent.next = node, 
     rest.length === 0 ? node :
         nodeify(node, rest))
 
-const buildCircularList = ([number, ...rest], head = { number }) => 
+const buildCircularList = ([number, ...rest], head = { number }) =>
     nodeify(head, rest).next = head
 
-const slice = (list, count, accumulator = [ ]) => 
+const slice = (list, count, accumulator = [ ]) =>
     count === 0 ? accumulator : 
         slice(list.next, count - 1, [ ...accumulator, list.number ])
 
@@ -21,17 +44,27 @@ const apply = (list, [number, ...rest]) => (
     rest.length === 0 ? list :
         apply(list.next, rest))
 
-const twist = (list, size) => 
+const twist = (list, size) =>
     size === 0 ? list :
         apply(list, slice(list, size).reverse()).next
 
-const skip = (list, count) => 
+const skip = (list, count) =>
     count === 0 ? list : 
         skip(list.next, count - 1)
 
-let head = buildCircularList(getList())
-getJumps().reduce((currentNode, twistLength, skipSize) => 
-    skip(twist(currentNode, twistLength), skipSize)
-, head)
+const generateHash = (list, size) =>
+    chunkArray(slice(head, 256))
+        .map(xor)
+        .map(tohex)
+        .join('')
 
-console.log(head.number * head.next.number)
+let head = buildCircularList(numberArray(256))
+let jumps = getHashJumps()
+let current = head
+let skipSize = 0
+
+numberArray(64).forEach(() =>
+    jumps.forEach(twistLength =>
+        current = skip(twist(current, twistLength), skipSize++)))
+
+console.log(generateHash(head))
