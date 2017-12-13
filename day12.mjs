17ce87f7ca95597793a17c87f8d38f45cf6b6653
Eric Lifka <eric.lifka@inin.com>
2017-12-12 20:21:45 -0500

    day12 part 1

--- day12.mjs
+++ day12.mjs
@@ -1,12 +1,25 @@
 import { splitChar 
        , getInput
        , removeComma
+       , parseDecimal
 } from './common'
 
 let programs =
     getInput('day12')
         .split('\n')
         .map(splitChar(' '))
-        .map(([program, indicator, ...pipes]) => pipes.map(removeComma))
+        .map(([program, indicator, ...pipes]) => pipes
+                                                    .map(removeComma)
+                                                    .map(parseDecimal))
 
-programs.forEach((pipes, program) => console.log(`${program} <-> ${pipes}`))
+let group = { }
+
+const visitProgram = ([ program, ...queue ]) =>
+    program === undefined ? null :
+        group[ program ] ? 
+            visitProgram(queue) :
+            ( group[ program ] = true,
+              visitProgram([ ...queue, ...programs[ program ]]) )
+
+visitProgram([ 0 ])
+console.log('part1', Object.keys(group).length)
