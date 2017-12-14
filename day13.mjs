7bc48f8058e5502219ed30c7d73f271ced81dfb6
Eric Lifka <eric.lifka@inin.com>
2017-12-13 22:33:19 -0500

    day 13 both parts

---
+++ day13.mjs
@@ -0,0 +1,32 @@
+import { getInput
+       , splitChar
+       , parseDecimal
+} from './common'
+
+const atHome = ([ depth, range ], delay = 0, time = depth + delay) =>
+    time % ( (range - 1) * 2 ) === 0
+
+const checkLayers = (delay, [ layer, ...rest ]) =>
+    layer === undefined ? true :
+        atHome(layer, delay) ? false :
+            checkLayers(delay, rest)
+
+let layers =
+getInput('day13')
+    .split('\n')
+    .map(splitChar(': '))
+    .map(([ depth, range ]) => 
+        [ parseDecimal(depth), parseDecimal(range) ])
+
+console.log('part1', 
+layers.reduce((sum, [ depth, range ]) => 
+    sum + atHome([ depth, range ]) ? 
+            depth * range : 
+            0
+, 0))
+
+for (var delay = 0; ; ++delay)
+    if (checkLayers(delay, layers))
+        break
+
+console.log('part2', delay)
